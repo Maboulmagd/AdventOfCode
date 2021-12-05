f8158f235732bd5fb8fe630359a8f2cf22f75d0b
Mohammed (Mo) Aboulmagd <Mohamedrefat007@hotmail.com>
2021-12-05 16:06:40 -0600

    Adding cpp solution for Day02

--- 2021/Day02/main.cpp
+++ 2021/Day02/main.cpp
@@ -1,19 +1,92 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
+#include <utility>
+
+void solve_a(const std::vector<std::pair<char, int>>& commands) {
+    int horizontal = 0;
+    int depth = 0;
+
+    for (const auto& command : commands) {
+        const char direction = command.first;
+        const int magnitude = command.second;
+
+        switch (direction) {
+            case 'f':
+                horizontal += magnitude;
+                break;
+            case 'u':
+                depth -= magnitude;
+                break;
+            case 'd':
+                depth += magnitude;
+                break;
+            default:
+                break;
+        }
+    }
+    
+    std::cout << "Product of final horizontal and depth position: " << horizontal * depth << std::endl;
+}
+
+void solve_b(const std::vector<std::pair<char, int>>& commands) {
+    int horizontal = 0;
+    int depth = 0;
+    int aim = 0;
+
+    for (const auto& command : commands) {
+        const char direction = command.first;
+        const int magnitude = command.second;
+
+        switch (direction) {
+            case 'f':
+                horizontal += magnitude;
+                depth += aim * magnitude;
+                break;
+            case 'u':
+                aim -= magnitude;
+                break;
+            case 'd':
+                aim += magnitude;
+                break;
+            default:
+                break;
+        }
+    }
+    
+    std::cout << "Product of AMMENDED final horizontal and depth position: " << horizontal * depth << std::endl;
+}
 
 int main() {
     std::ifstream input_file("input.txt");
 
-    std::vector<int> res;
+    std::vector<std::pair<char, int>> res;
 
     std::string line;
     while (std::getline(input_file, line)) {
-        res.push_back(std::stoi(line));
+        std::size_t pos = line.find("forward");
+        int magnitude = 0;
+
+        if (pos != std::string::npos) {
+            magnitude = std::stoi(line.substr(8));
+            res.push_back(std::make_pair('f', magnitude));
+        }
+
+        pos = line.find("down");
+        if (pos != std::string::npos) {
+            magnitude = std::stoi(line.substr(5));
+            res.push_back(std::make_pair('d', magnitude));
+        }
+
+        pos = line.find("up");
+        if (pos != std::string::npos) {
+            magnitude = std::stoi(line.substr(3));
+            res.push_back(std::make_pair('u', magnitude));
+        }
     }
 
-    // solve_a(res);
-    // solve_b(res);
+    solve_a(res);
+    solve_b(res);
     
     return 0;
 }
