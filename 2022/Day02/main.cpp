6e243917de46f747ad46a2cab9be9c92f3c579fb
Maboulmagd <Mohamedrefat007@gmail.com>
2022-12-09 00:34:25 -0600

    Adding part1 of Day02 Solution
    This builds on Windows.
    Should try to get it building and running in MacOS homebrew Clang.

--- 2022/Day02/main.cpp
+++ 2022/Day02/main.cpp
@@ -1,22 +1,21 @@
 #include <fstream>
 #include <vector>
 #include <utility>
-// #include <string>
 #include <algorithm>
 #include <iostream>
 #include <ranges>
 #include <numeric>
 
-constexpr void FollowStrategyGuide(const std::vector<std::pair<char,char>>& strategy_guide) {
-    auto round_score = [](const auto& pair) {
-        const int elf = pair.first - 'X';
+constexpr int FollowStrategyGuide(const std::vector<std::pair<char,char>>& strategy_guide) {
+    const auto round_score = [](const auto& pair) {
+        const int elf = pair.first - 'A';
         const int me = pair.second - 'X';
 
         int my_score = me + 1;
         if (me == elf) {
             my_score += 3;
         }
-        else if (((me - elf + 3) % 3) == 0) {
+        else if (((me - elf + 3) % 3) == 1) {
             my_score += 6;
         }
 
@@ -24,21 +23,21 @@ constexpr void FollowStrategyGuide(const std::vector<std::pair<char,char>>& stra
     };
 
     const auto scores = strategy_guide | std::views::transform(round_score) | std::views::common;
-    return std::reduce(scores.cbegin(), scores.cend());
+    return std::reduce(scores.begin(), scores.end());
 }
 
 int main() {
-    std::fstream input_file("input.txt");
+    std::fstream input_file("2022\\Day02\\input.txt");
 
     std::vector<std::pair<char, char>> input;
 
     if (!input_file.is_open()) {
-        std::cerr << "Failed to open " << std::quoted(path) << '\n';
+        //std::cerr << "Failed to open " << std::quoted(path) << '\n';
         return 1;
     }
 
     std::pair<char, char> current_round;
-    while (input >> current_round.first >> current_round.second) {
+    while (input_file >> current_round.first >> current_round.second) {
         input.push_back(current_round);
     }
 
