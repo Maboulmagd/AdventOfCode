990e813ffee816414a5704e62878f08b7b317950
Mohammed (Mo) Aboulmagd <Mohamedrefat007@hotmail.com>
2022-12-13 23:42:09 -0600

    Adding solution for part one of Day03
    This is the first draft to part one.

---
+++ 2022/Day03/main.cpp
@@ -0,0 +1,71 @@
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <ranges>
+#include <numeric>
+#include <cassert>
+#include <unordered_set>
+
+constexpr int GetPrioritySum(const std::vector<std::string>& input) {
+    std::vector<int> priority_score(52);
+    std::iota(priority_score.begin(), priority_score.end(), 1);
+
+    const auto get_priority = [&priority_score](const auto& line){
+        assert(line.length() % 2 == 0);
+
+        const size_t num_items = line.length();
+
+        const std::string& first_compartment = line.substr(0, num_items / 2);
+        const std::string& second_compartment = line.substr(num_items / 2);
+
+        assert(first_compartment.length() == second_compartment.length());
+
+        std::unordered_set<char> found;
+        found.reserve(first_compartment.length());
+
+        for (const char& c : first_compartment) {
+            found.insert(c);
+        }
+
+        char common = '\0';
+
+        for (const char& c : second_compartment) {
+            if (found.find(c) != found.cend()) {
+                assert(common == '\0');
+                common = c;
+                break;
+            }
+        }
+
+        if (common >= 'a' && common <= 'z') {
+            return priority_score[common - 'a'];
+        }
+
+        return priority_score[common - 'A' + 26];
+    };
+
+    const auto priorities = input | std::views::transform(get_priority) | std::views::common;
+    return std::reduce(priorities.begin(), priorities.end());
+}
+
+int main() {
+    std::fstream input_file("input.txt");
+
+    std::vector<std::string> input;
+
+    if (!input_file.is_open()) {
+        //std::cerr << "Failed to open " << std::quoted(path) << '\n';
+        return 1;
+    }
+
+    std::string line;
+    while (std::getline(input_file, line)) {
+        input.push_back(line);
+    }
+
+    std::cout << GetPrioritySum(input) << std::endl;
+
+
+    return 0;
+}
