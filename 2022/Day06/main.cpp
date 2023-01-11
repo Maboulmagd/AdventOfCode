5f48088f1e58b4b86c85926807daf189aa4360bb
Mohammed (Mo) Aboulmagd <Mohamedrefat007@hotmail.com>
2023-01-11 02:21:20 -0600

    Adding Test() function for sanity checks/basic testing to Day06 solution
    Also changed input from std::vector<std::string> to simply std::string.

--- 2022/Day06/main.cpp
+++ 2022/Day06/main.cpp
@@ -6,10 +6,8 @@
 #include <cassert>
 #include <unordered_map>
 
- uint32_t GetNumberOfCharactersForFirstMarker(const std::vector<std::string>& input, const std::size_t marker = 4) {
-    assert(input.size() == 1);// Sanity check
-    const std::string_view data_stream = input[0];
-    const std::size_t data_len = data_stream.length();
+ uint32_t GetNumberOfCharactersForFirstMarker(const std::string& input, const std::size_t marker = 4) {
+    const std::size_t data_len = input.length();
 
     std::unordered_map<char, uint32_t> char_freq;
     char_freq.reserve(26);
@@ -17,16 +15,16 @@
     std::size_t i = 0;
     std::size_t j = 0;
     for (; j < data_len; ++j) {
-        char_freq[data_stream[j]]++;// Increment count/frequency
+        char_freq[input[j]]++;// Increment count/frequency
 
         if (j+1 >= marker) {
             if (char_freq.size() == marker) {
                 break;
             }
 
-            char_freq[data_stream[i]]--;// Decrement count/frequency of character leaving window
-            if (char_freq[data_stream[i]] == 0) {// Remove character from our map, to reduce size
-                char_freq.erase(data_stream[i]);
+            char_freq[input[i]]--;// Decrement count/frequency of character leaving window
+            if (char_freq[input[i]] == 0) {// Remove character from our map, to reduce size
+                char_freq.erase(input[i]);
             }
 
             ++i;
@@ -44,12 +42,8 @@ int ParseAndRun(const std::string& path) {
         return 1;
     }
 
-    std::vector<std::string> input;
-
-    std::string line;
-    while (std::getline(file_name, line)) {
-        input.push_back(line);
-    }
+    std::string input;
+    std::getline(file_name, input);
 
     std::cout << GetNumberOfCharactersForFirstMarker(input) << '\n';// Start of packet marker
     std::cout << GetNumberOfCharactersForFirstMarker(input, 14) << '\n';// Start of message marker
@@ -57,9 +51,25 @@ int ParseAndRun(const std::string& path) {
     return 0;
 }
 
+uint32_t Test() {
+    std::string t1 = "bvwbjplbgvbhsrlpgdmjqwftvncz"; // 5
+    assert(GetNumberOfCharactersForFirstMarker(t1) == 5);
+    assert(GetNumberOfCharactersForFirstMarker(t1, 14) == 23);
+    std::string t2 = "nppdvjthqldpwncqszvftbrmjlhg"; // 6
+    assert(GetNumberOfCharactersForFirstMarker(t2) == 6);
+    assert(GetNumberOfCharactersForFirstMarker(t2, 14) == 23);
+    std::string t3 = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"; // 10
+    assert(GetNumberOfCharactersForFirstMarker(t3) == 10);
+    assert(GetNumberOfCharactersForFirstMarker(t3, 14) == 29);
+    std::string t4 = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"; // 11
+    assert(GetNumberOfCharactersForFirstMarker(t4) == 11);
+    assert(GetNumberOfCharactersForFirstMarker(t4, 14) == 26);
+    return 0;
+}
+
 int main(int argc, char** argv) {
     if (argc == 1) {
-        return 1;
+        return Test();
     }
 
     return ParseAndRun(argv[1]);
