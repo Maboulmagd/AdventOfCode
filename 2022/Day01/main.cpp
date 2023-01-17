014417e491eb57584e3a6f0596f73a0b78d7844a
Mohammed (Mo) Aboulmagd <Mohamedrefat007@hotmail.com>
2023-01-16 20:15:55 -0600

    Refactored Day01 solutions
    Added a Test() function for basic input testing capability.
    Refactored both part one and two solutions to use std::views and std::ranges.

--- 2022/Day01/main.cpp
+++ 2022/Day01/main.cpp
@@ -1,40 +1,79 @@
-#include <fstream>
 #include <string>
-#include <vector>
-#include <algorithm>
+#include <fstream>
+#include <iomanip>
 #include <iostream>
-#include <ranges>
+#include <vector>
+#include <cassert>
+#include <algorithm>
 #include <numeric>
+#include <ranges>
 
-constexpr int GetMaxCalories(const std::vector<int>& calories_by_elf) {
-    return *max_element(calories_by_elf.cbegin(), calories_by_elf.cend());
+constexpr uint64_t GetMaxCalories(const std::vector<std::string>& data) {
+    const auto calories_by_elf = data |
+            std::views::lazy_split(std::string{}) |// Splits container's data on empty string (empty line from input), as we want to group by elf
+            std::views::transform([](const auto& elf) -> uint64_t {// Sum up the calories for each elf
+                const auto to_unsigned = [](const std::string& in) { return std::stoull(in); };
+                const auto rng = elf |
+                        std::views::transform(to_unsigned) |
+                        std::views::common;
+                return std::reduce(rng.begin(), rng.end());
+            });
+    return std::ranges::max(calories_by_elf);
 }
 
-int GetTopThreeElvesCalories(const std::vector<int>& calories_by_elf) {
-    std::vector<int> top_three(3);
-    std::partial_sort_copy(calories_by_elf.begin(), calories_by_elf.end(), top_three.begin(), top_three.end(), std::greater<int>{});
-    return std::reduce(top_three.cbegin(), top_three.cend());
+constexpr uint64_t GetTopThreeElvesCalories(const std::vector<std::string>& data) {
+    const auto calories_by_elf = data |
+            std::views::lazy_split(std::string{}) |// Splits container's data on empty string (empty line from input), as we want to group by elf
+            std::views::transform([](const auto& elf) -> uint64_t {// Sum up the calories for each elf
+                const auto to_unsigned = [](const std::string& in) { return std::stoull(in); };
+                const auto rng = elf |
+                        std::views::transform(to_unsigned) |
+                        std::views::common;
+                    return std::reduce(rng.begin(), rng.end());
+            });
+    std::vector<uint64_t> top_three_elves(3);
+    std::ranges::partial_sort_copy(calories_by_elf, top_three_elves, std::greater<>());
+    return std::reduce(top_three_elves.cbegin(), top_three_elves.cend());
 }
 
-int main() {
-    std::fstream input_file("input.txt");
-
-    std::vector<int> calories_by_elf;
-    int curr_elf_calories = 0;
-
-    std::string line;
-    while (std::getline(input_file, line)) {
-        if (line.empty()) {
-            calories_by_elf.push_back(curr_elf_calories);
-            curr_elf_calories = 0;
-        }
-        else {
-            curr_elf_calories += stoi(line);
-        }
-    }
-
-    std::cout << GetMaxCalories(calories_by_elf) << std::endl;
-    std::cout << GetTopThreeElvesCalories(calories_by_elf) << std::endl;
+int Test() {
+    std::vector<std::string> test_data{
+            "1000", "2000", "3000", "",
+            "4000", "",
+            "5000", "6000", "",
+            "7000", "8000", "9000", "",
+            "10000"
+    };
+    assert(GetMaxCalories(test_data) == 24000);
+    assert(GetTopThreeElvesCalories(test_data) == 45000);
 
     return 0;
+}
+
+int ParseAndRun(const std::string& path) {
+    std::fstream input_file(path);
+
+    if (!input_file.is_open()) {
+        std::cerr << "Failed to open " << std::quoted(path) << "\n";
+        return 1;
+    }
+
+    std::vector<std::string> data;
+    std::string line;
+    while (std::getline(input_file, line)) {
+        data.push_back(line);
+    }
+
+    std::cout << GetMaxCalories(data) << std::endl;
+    std::cout << GetTopThreeElvesCalories(data) << std::endl;
+
+    return 0;
+}
+
+int main(int argc, char** argv) {
+    if (argc == 1) {
+        return Test();
+    }
+
+    return ParseAndRun(argv[1]);
 }
