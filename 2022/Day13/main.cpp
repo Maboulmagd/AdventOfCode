845c3bb12a74d28f37921d0def88a39005f98b14
Maboulmagd <Mohamedrefat007@gmail.com>
2023-02-23 23:55:57 -0600

    Adding Day13 part one solution
    Been a week or so since I looked at this. Haven't had my laptop, waiting
    on new one. Finishing this up on Visual Studio 2022 on my Windows machine.
    
    Also updated .gitignore.

--- 2022/Day13/main.cpp
+++ 2022/Day13/main.cpp
@@ -5,8 +5,24 @@
 #include <iostream>
 #include <iomanip>
 #include <utility>
+#include <sstream>
+#include <cassert>
 
 using namespace AOC2022Day13Packet;
+
+uint32_t NumberOfPacketsInRightOrder(const std::vector<std::pair<Packet, Packet>>& packet_pairs) {
+    uint32_t res = 0;
+    uint32_t index = 1;
+
+    for (const auto& packet_pair : packet_pairs) {
+        if (packet_pair.first < packet_pair.second) {
+            res += index;
+        }
+        ++index;
+    }
+
+    return res;
+}
 
 int ParseAndRun(const std::string& path) {
     std::fstream file_stream(path);
@@ -16,17 +32,52 @@ int ParseAndRun(const std::string& path) {
         return 1;
     }
 
-    std::vector<std::pair<Packet, Packet>> packet_pairs_;
+    std::vector<std::pair<Packet, Packet>> packet_pairs;
     Packet a;
     Packet b;
     while (file_stream >> a >> b) {
-        packet_pairs_.emplace_back(std::move(a), std::move(b));
+        packet_pairs.emplace_back(std::move(a), std::move(b));
     }
+
+    std::cout << NumberOfPacketsInRightOrder(packet_pairs) << std::endl;
 
     return 0;
 }
 
 int Test() {
+    std::stringstream ss(R"([1,1,3,1,1]
+[1, 1, 5, 1, 1]
+    
+[[1], [2, 3, 4]]
+[[1], 4]
+
+[9]
+[[8, 7, 6]]
+
+[[4, 4], 4, 4]
+[[4, 4], 4, 4, 4]
+
+[7, 7, 7, 7]
+[7, 7, 7]
+
+[]
+[3]
+
+[[[]] ]
+[[]]
+
+[1, [2, [3, [4, [5, 6, 7]]]], 8, 9]
+[1, [2, [3, [4, [5, 6, 0]]]], 8, 9])");
+
+    std::vector<std::pair<Packet, Packet>> packet_pairs;
+    Packet a;
+    Packet b;
+    while (ss >> a >> b) {
+        packet_pairs.emplace_back(std::move(a), std::move(b));
+    }
+
+    assert(NumberOfPacketsInRightOrder(packet_pairs) == 13);
+
     return 0;
 }
 
--- 2022/Day13/packet.h
+++ 2022/Day13/packet.h
@@ -13,14 +13,91 @@ namespace AOC2022Day13Packet {
         std::vector<Token*> content_;
         std::optional<int32_t> integer_;
 
-        explicit Token(Token* parent, const int32_t integer) : parent_(parent), integer_(integer) {}
+        explicit Token(Token* parent, int32_t integer) : parent_(parent), integer_(integer) {}
         explicit Token(Token* parent) : parent_(parent) {}
 
+        // This function needs to be defined first
+        friend auto operator<=>(int32_t value, const Token& right) {
+            // Base case, won't hit this on very first call here
+            if (right.integer_) {
+                return value <=> *right.integer_;
+            }
+            // Comparing empty list with integer
+            if (right.content_.empty()) {
+                return std::strong_ordering::greater;
+            }
+            // Comparing an integer against a non-empty list, so recurse
+            const auto cmp = value <=> *right.content_[0];
+            if (std::is_eq(cmp)) {
+                // 2 <=> [2, .... ]
+                if (right.content_.size() == 1) {
+                    return std::strong_ordering::equal;
+                }
+                return std::strong_ordering::less;
+            }
+            return cmp;
+        }
+
+        // This function needs to be defined second
+        friend auto operator<=>(const Token& left, int32_t value) {
+            // Symmetrical
+            const auto cmp = value <=> left;
+            if (std::is_lt(cmp)) {
+                return std::strong_ordering::greater;
+            }
+            if (std::is_gt(cmp)) {
+                return std::strong_ordering::less;
+            }
+            return std::strong_ordering::equal;
+        }
+
+        // I started out by writing this function first, since this is where the chain of execution begins
+        friend auto operator<=>(const Token& left, const Token& right) {
+            if (left.integer_ && right.integer_) {
+                return *left.integer_ <=> *right.integer_;
+            }
+            if (left.integer_) {
+                return *left.integer_ <=> right;
+            }
+            if (right.integer_) {
+                return left <=> *right.integer_;
+            }
+
+            //std::lexicographical_compare_three_way()
+            auto left_it = left.content_.begin();
+            auto right_it = right.content_.begin();
+
+            while (true) {
+                if (left_it == left.content_.end() && right_it == right.content_.end()) {
+                    return std::strong_ordering::equal;
+                }
+                if (left_it == left.content_.end()) {
+                    return std::strong_ordering::less;// Left side ran out first, so inputs are in the right order, return less
+                }
+                if (right_it == right.content_.end()) {
+                    return std::strong_ordering::greater;
+                }
+
+                const auto cmp = **left_it <=> **right_it;// Recurse either on this same function, or the other ones
+                if (!std::is_eq(cmp)) {
+                    return cmp;
+                }
+
+                ++left_it;
+                ++right_it;
+            }
+
+            return std::strong_ordering::equal;
+        }
     };
 
     struct Packet final {
         std::vector<std::unique_ptr<Token>> tokens_;
         Token* root_ = nullptr;
+
+        auto operator<=>(const Packet& packet) const {
+            return *root_ <=> *packet.root_;
+        }
 
         Token* NewIntegerToken(Token* parent, const int32_t integer) {
             tokens_.push_back(std::make_unique<Token>(parent, integer));
